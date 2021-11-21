ac1bc25d0ab81c64d9462ef46e83d1b78d27a1a7
R. Miles McCain <github@sendmiles.email>
2021-11-21 14:16:40 -0800

    Add exact and fuzzy clustering

--- src/cluster.py
+++ src/cluster.py
@@ -5,47 +5,120 @@ import nltk
 import tempfile
 from numpy import ndarray
 from loguru import logger
-from pyclustering.cluster import optics
+from collections import defaultdict
 import hdbscan
-from fasttext.FastText import _FastText as FastTextModel
+
 
 def preprocess_text(text: str) -> str:
     return " ".join(nltk.word_tokenize(text.lower()))
 
-def train_word_vectors(post_texts: List[str]) -> FastTextModel:
-    logger.info("Training word vectors...")
-    fd, path = tempfile.mkstemp()
-    with fdopen(fd, "wb") as outfile:
-        for text in post_texts:
-            out = preprocess_text(text) + "\n"
-            outfile.write(out.encode("utf8"))
 
-    model = fasttext.train_unsupervised(path, model="skipgram")
-    remove(path)
-    logger.info("Word vectors trained!")
-    return model
+class TextClusterer:
+    def fit(self, texts: List[str]) -> None:
+        raise NotImplemented
 
-def _vectorize(text: str, model: FastTextModel) -> ndarray:
-    return model.get_sentence_vector(preprocess_text(text))
+    def clusters(self) -> List[int]:
+        raise NotImplemented
 
-def cluster_texts(texts: List[str], model: FastTextModel) -> ndarray:
-    logger.info("Clustering texts...")
-    vectors = [_vectorize(text, model) for text in texts]
-    clusters = optics.optics(vectors, 0.05, 5, ccore=True)
-    clusters.process()
-    predictions = clusters.get_clusters()
-    logger.info("Clustering complete!")
-    return predictions
+    def probabilities(self) -> List[float]:
+        raise NotImplemented
 
-def display_clusters(texts: List[str], clusters: List[List[int]]):
-    for i in range(len(clusters)):
-        print(f"Cluster #{i+1} -----")
-        for idx in clusters[i]:
-            print(f"{idx}: {texts[idx]}")
+    def outlier_probabilities(self) -> List[float]:
+        raise NotImplemented
+
+
+class FuzzyClusterer(TextClusterer):
+    def __init__(self, dims=25, min_cluster_size=3, min_samples=3):
+        self.dims = dims
+        self.min_cluster_size = min_cluster_size
+        self.min_samples = min_samples
+        self._model = None
+
+    def _train_word_vectors(
+        self,
+        texts: List[str],
+    ) -> None:
+        logger.info("Training word vectors...")
+        fd, path = tempfile.mkstemp()
+        with fdopen(fd, "wb") as outfile:
+            for text in texts:
+                out = preprocess_text(text) + "\n"
+                outfile.write(out.encode("utf8"))
+
+        model = fasttext.train_unsupervised(path, model="skipgram", dim=self.dims)
+        remove(path)
+        logger.info("Word vectors trained!")
+        self._model = model
+
+    def _vectorize(self, text: str) -> ndarray:
+        return self.model.get_sentence_vector(preprocess_text(text))
+
+    def _cluster_texts(self, texts: List[str]) -> None:
+        logger.info("Clustering texts...")
+        vectors = [self._vectorize(text, model) for text in texts]
+        clusters = hdbscan.HDBSCAN(
+            min_cluster_size=self.min_cluster_size, min_samples=self.min_samples
+        )
+        self._predictions = clusters.fit_predict(vectors)
+        logger.info("Clustering complete!")
+        self._clusterer = clusters
+
+    def fit(self, texts: List[str]) -> None:
+        self._train_word_vectors(texts)
+        self._cluster_texts()
+
+    def clusters(self) -> List[int]:
+        return self._predictions
+
+    def probabilities(self) -> List[float]:
+        return self._clusterer.probabilities_
+
+    def outlier_probabilities(self) -> List[float]:
+        return self._clusterer.outlier_scores_
+
+
+class ExactClusterer:
+    def fit(self, texts: List[str]) -> None:
+        dupes = defaultdict(lambda: [])
+        for idx, text in enumerate(texts):
+            dupes[preprocess_text(text)].append(idx)
+
+        clusters = {}
+        i = 0
+        for _, occurences in dupes.items():
+            if len(occurences) <= 1:
+                continue
+            for occurence in occurences:
+                clusters[occurence] = i
+            i += 1
+
+        self._clusters = [clusters.get(i, -1) for i in range(len(texts))]
+
+    def clusters(self) -> List[int]:
+        return self._clusters
+
+    def probabilities(self) -> List[float]:
+        return [1 if c != -1 else 0 for c in self.clusters()]
+
+    def outlier_probabilities(self) -> List[float]:
+        return [1 if c == -1 else 0 for c in self.clusters()]
+
+
+def _display_clusters(
+    texts: List[str], clusters: List[List[int]], probabilities: List[float]
+):
+    cluster_assignments = defaultdict(lambda: [])
+    for i, assignment in enumerate(clusters):
+        cluster_assignments[assignment].append(i)
+    for cluster, indices in cluster_assignments.items():
+        print(f"Cluster {cluster} -----")
+        for idx in indices:
+            print(f"{cluster} {probabilities[idx]}: {texts[idx].strip()}")
+
 
 if __name__ == "__main__":
-    with open("data/buzz.txt", "r") as infile:
+    with open("data/buzz_full.txt", "r") as infile:
         lines = infile.readlines()
-    model = train_word_vectors(lines)
-    cs = cluster_texts(lines, model)
-    display_clusters(lines, cs)
+    cl = ExactClusterer()
+    cl.fit(lines)
+    _display_clusters(lines, cl.clusters(), cl.probabilities())
